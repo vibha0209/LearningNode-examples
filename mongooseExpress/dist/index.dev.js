0d90ccc453fb29fbb26e5d40d8cfd0ba6e1fbab2
vibha <vibhaverma@live.com>
2021-05-04 12:19:30 +0530

    Node

--- mongooseExpress/dist/index.dev.js
+++ mongooseExpress/dist/index.dev.js
@@ -8,7 +8,18 @@ var path = require('path');
 
 var Product = require('./models/product');
 
+var methodOverride = require('method-override');
+
+app.use(methodOverride('_method'));
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
+
 var mongoose = require('mongoose');
+
+var _require = require('dns'),
+    ALL = _require.ALL;
 
 mongoose.connect('mongodb://localhost:27017/farmStand', {
   useNewUrlParser: true,
@@ -21,8 +32,169 @@ mongoose.connect('mongodb://localhost:27017/farmStand', {
 });
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.get('/dog', function (req, res) {
-  res.send('woof');
+var categories = ['fruit', 'vegetable', 'dairy'];
+app.get('/products', function _callee(req, res) {
+  var category, products, _products;
+
+  return regeneratorRuntime.async(function _callee$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          category = req.query.category;
+
+          if (!category) {
+            _context.next = 8;
+            break;
+          }
+
+          _context.next = 4;
+          return regeneratorRuntime.awrap(Product.find({
+            category: category
+          }));
+
+        case 4:
+          products = _context.sent;
+          res.render('products/index', {
+            products: products,
+            category: category
+          });
+          _context.next = 12;
+          break;
+
+        case 8:
+          _context.next = 10;
+          return regeneratorRuntime.awrap(Product.find({}));
+
+        case 10:
+          _products = _context.sent;
+          res.render('products/index', {
+            products: _products,
+            category: 'ALL'
+          });
+
+        case 12:
+        case "end":
+          return _context.stop();
+      }
+    }
+  });
+});
+app.get('/products/new', function (req, res) {
+  res.render('products/new', {
+    categories: categories
+  });
+});
+app.post('/products', function _callee2(req, res) {
+  var newProduct;
+  return regeneratorRuntime.async(function _callee2$(_context2) {
+    while (1) {
+      switch (_context2.prev = _context2.next) {
+        case 0:
+          newProduct = new Product(req.body);
+          _context2.next = 3;
+          return regeneratorRuntime.awrap(newProduct.save());
+
+        case 3:
+          res.redirect("/products/".concat(newProduct._id));
+
+        case 4:
+        case "end":
+          return _context2.stop();
+      }
+    }
+  });
+});
+app.get('/products/:id', function _callee3(req, res) {
+  var id, product;
+  return regeneratorRuntime.async(function _callee3$(_context3) {
+    while (1) {
+      switch (_context3.prev = _context3.next) {
+        case 0:
+          id = req.params.id;
+          _context3.next = 3;
+          return regeneratorRuntime.awrap(Product.findById(id));
+
+        case 3:
+          product = _context3.sent;
+          res.render('products/show', {
+            product: product
+          });
+
+        case 5:
+        case "end":
+          return _context3.stop();
+      }
+    }
+  });
+});
+app.get('/products/:id/edit', function _callee4(req, res) {
+  var id, product;
+  return regeneratorRuntime.async(function _callee4$(_context4) {
+    while (1) {
+      switch (_context4.prev = _context4.next) {
+        case 0:
+          id = req.params.id;
+          _context4.next = 3;
+          return regeneratorRuntime.awrap(Product.findById(id));
+
+        case 3:
+          product = _context4.sent;
+          res.render('products/edit', {
+            product: product,
+            categories: categories
+          });
+
+        case 5:
+        case "end":
+          return _context4.stop();
+      }
+    }
+  });
+});
+app.put('/products/:id', function _callee5(req, res) {
+  var id, product;
+  return regeneratorRuntime.async(function _callee5$(_context5) {
+    while (1) {
+      switch (_context5.prev = _context5.next) {
+        case 0:
+          id = req.params.id;
+          _context5.next = 3;
+          return regeneratorRuntime.awrap(Product.findByIdAndUpdate(id, req.body, {
+            runValidators: true,
+            "new": true
+          }));
+
+        case 3:
+          product = _context5.sent;
+          res.redirect("/products/".concat(product._id));
+
+        case 5:
+        case "end":
+          return _context5.stop();
+      }
+    }
+  });
+});
+app["delete"]('/products/:id', function _callee6(req, res) {
+  var id, deleteProduct;
+  return regeneratorRuntime.async(function _callee6$(_context6) {
+    while (1) {
+      switch (_context6.prev = _context6.next) {
+        case 0:
+          id = req.params.id;
+          _context6.next = 3;
+          return regeneratorRuntime.awrap(Product.findByIdAndDelete(id));
+
+        case 3:
+          deleteProduct = _context6.sent;
+          res.redirect("/products");
+
+        case 5:
+        case "end":
+          return _context6.stop();
+      }
+    }
+  });
 });
 app.listen(3000, function () {
   console.log("App listening on port : 3000");
--- mongooseExpress/index.js
+++ mongooseExpress/index.js
@@ -2,8 +2,15 @@ const express=require('express');
 const app = express();
 const path=require('path');
 const Product=require('./models/product')
+const methodOverride = require('method-override')
+app.use(methodOverride('_method'))
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
 
 const mongoose = require('mongoose');
+const { ALL } = require('dns');
 mongoose.connect('mongodb://localhost:27017/farmStand', {useNewUrlParser: true, useUnifiedTopology: true})
 .then(()=>{
     console.log("Mongo Connection Open!!!")
@@ -17,9 +24,63 @@ mongoose.connect('mongodb://localhost:27017/farmStand', {useNewUrlParser: true, 
 app.set('views', path.join(__dirname,'views'))
 app.set('view engine', 'ejs');
 
-app.get('/dog',(req,res)=>{
-    res.send('woof')
+const categories = ['fruit', 'vegetable', 'dairy'];
+
+app.get('/products', async (req,res) => {
+    const { category } = req.query;
+    if(category){
+      const products= await Product.find({category});
+      res.render('products/index', {products, category})
+    } else {
+        const products= await Product.find({});
+        res.render('products/index', {products, category:'ALL'})
+    }
+   
 })
+app.get('/products/new', (req,res) =>{
+    res.render('products/new',{categories});
+})
+
+app.post('/products', async (req,res) =>{
+    const newProduct=new Product(req.body);
+    await newProduct.save();
+    res.redirect(`/products/${newProduct._id}`)
+})
+
+
+
+
+app.get('/products/:id' , async (req, res) =>{
+    const { id } = req.params;
+    const product =await Product.findById(id);
+    res.render('products/show', {product})
+})
+app.get('/products/:id/edit' , async (req, res) =>{
+    const { id } = req.params;
+    const product =await Product.findById(id);
+    res.render('products/edit', {product,categories})
+})
+
+
+app.put('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true });
+    res.redirect(`/products/${product._id}`);
+})
+
+app.delete('/products/:id', async (req,res)=>{
+    const { id } = req.params;
+    const deleteProduct = await Product.findByIdAndDelete(id);
+    res.redirect(`/products`);
+
+})
+
+
+
+
+
+
+
 app.listen(3000, ()=>{
     console.log("App listening on port : 3000")
 })
